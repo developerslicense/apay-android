c0560474ace51390b4369392ae11ef4e7c60634e
Mikhail <mikhail.belikov@airbapay.kz>
2023-07-31 15:51:10 +0600

    перенес часть кода из флаттера

---
+++ apay_android/src/test/java/kz/airbapay/apay_android/test_for_data/GetNumberClearedWithMaxSizeTests.kt
@@ -0,0 +1,76 @@
+package kz.airbapay.apay_android.test_for_data
+
+import kz.airbapay.apay_android.data.constant.kzt
+import kz.airbapay.apay_android.data.utils.getNumberClearedWithMaxSymbol
+import kz.airbapay.apay_android.isAssert
+import org.junit.Test
+
+class GetNumberClearedWithMaxSizeTests {
+
+  private fun testGetNumberCleared(
+    amount: String?,
+    isUserEntered: Boolean = false
+  ) : String {
+    return getNumberClearedWithMaxSymbol(amount, isUserEntered =  isUserEntered)
+  }
+
+  @Test
+  fun testsGetNumberClearedWithMaxSize() {
+
+      isAssert(testGetNumberCleared("±§!@#\$&*:[]`~%^&100000.0"), "100000")
+      isAssert(testGetNumberCleared("100000&*()^.0"), "100000")
+      isAssert(testGetNumberCleared("100000&*()^,0"), "100000")
+      isAssert(testGetNumberCleared("100000&*()^.00"), "100000")
+      isAssert(testGetNumberCleared("100000&*()^,00"), "100000")
+      isAssert(testGetNumberCleared("100000&*()^"), "100000")
+      isAssert(testGetNumberCleared("1000&*()^00"), "100000")
+
+      isAssert(testGetNumberCleared("±§!@#\$&*:[]`~%^&100000.0", isUserEntered =true), "1000000")
+      isAssert(testGetNumberCleared("100000&*()^.0", isUserEntered = true), "1000000")
+      isAssert(testGetNumberCleared("100000&*()^,0", isUserEntered = true), "1000000")
+      isAssert(testGetNumberCleared("100000&*()^.00", isUserEntered = true), "10000000")
+      isAssert(testGetNumberCleared("100000&*()^,00", isUserEntered = true), "10000000")
+      isAssert(testGetNumberCleared("100000&*()^", isUserEntered = true), "100000")
+      isAssert(testGetNumberCleared("1000&*()^00", isUserEntered = true), "100000")
+
+      isAssert(testGetNumberCleared(" 1 234 567 890 ${kzt}", isUserEntered = true), "1234567890")
+      isAssert(testGetNumberCleared(" 1 234 567 890 ${kzt}", isUserEntered = false), "1234567890")
+
+      testWithMinus()
+
+  }
+
+
+  private fun testWithMinus() {
+    isAssert(testGetNumberCleared("-100000"), "-100000")
+    isAssert(testGetNumberCleared("--100000"), "-100000")
+    isAssert(testGetNumberCleared("--100-000"), "-100000")
+    isAssert(testGetNumberCleared("-100000.0"), "-100000")
+    isAssert(testGetNumberCleared("-1000zXьЛ00"), "-100000")
+    isAssert(testGetNumberCleared("1-000zXьЛ00"), "100000")
+    isAssert(testGetNumberCleared("1000-zXьЛ00"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ-00"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00-"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-0"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-00"), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-0-0"), "100000")
+    isAssert(testGetNumberCleared("-1000zXьЛ00.-0-0"), "-100000")
+
+    isAssert(testGetNumberCleared("-100000", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("--100000", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("--100-000", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("-100000.0", isUserEntered =  true), "1000000")
+    isAssert(testGetNumberCleared("-1000zXьЛ00", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1-000zXьЛ00", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1000-zXьЛ00", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ-00", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00-", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-", isUserEntered =  true), "100000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-0", isUserEntered =  true), "1000000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-00", isUserEntered =  true), "10000000")
+    isAssert(testGetNumberCleared("1000zXьЛ00.-0-0", isUserEntered =  true), "10000000")
+    isAssert(testGetNumberCleared("-1000zXьЛ00.-0-0", isUserEntered =  true), "10000000")
+
+  }
+}
