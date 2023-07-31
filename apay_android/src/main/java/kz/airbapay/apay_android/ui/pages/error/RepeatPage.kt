f7c99422a39b4b8050c0e51b76df346370b2ab8d
Mikhail <mikhail.belikov@airbapay.kz>
2023-07-31 16:37:18 +0600

    перенес код из флаттера

---
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/error/RepeatPage.kt
@@ -0,0 +1,79 @@
+package kz.airbapay.apay_android.ui.pages.error
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clipToBounds
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import kz.airbapay.apay_android.data.constant.thisNeedSomeTime
+import kz.airbapay.apay_android.data.constant.weRepeatYourPayment
+import kz.airbapay.apay_android.ui.resources.ColorsSdk
+import kz.airbapay.apay_android.ui.resources.LocalFonts
+
+@Composable
+internal fun RepeatPage() {
+
+    ConstraintLayout(
+        modifier = Modifier
+            .background(ColorsSdk.bgMain)
+            .clipToBounds()
+            .fillMaxSize()
+    ) {
+
+        val (spaceRef, progressRef, textRef, text2Ref) = createRefs()
+
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight(0.35f)
+                .constrainAs(spaceRef) {
+                    top.linkTo(parent.top)
+                }
+        )
+
+        Text(
+            text = weRepeatYourPayment(),
+            style = LocalFonts.current.h3,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .constrainAs(textRef) {
+                    top.linkTo(spaceRef.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        Text(
+            text = thisNeedSomeTime(),
+            style = LocalFonts.current.regular,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .constrainAs(text2Ref) {
+                    top.linkTo(textRef.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+
+        CircularProgressIndicator(
+            color = ColorsSdk.buttonMainBrandMS.value,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .fillMaxWidth(0.3f)
+                .constrainAs(progressRef) {
+                    top.linkTo(text2Ref.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+    }
+}
