f9bd9a9e85bd8d95fe83c188a5ddeab916a1d335
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-01 18:17:08 +0600

    перенес код из флаттера

--- apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/home/HomePage.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/home/HomePage.kt
@@ -12,7 +12,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -24,6 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kz.airbapay.apay_android.R
+import kz.airbapay.apay_android.data.constant.cardDataSaved
 import kz.airbapay.apay_android.data.constant.paymentOfPurchase
 import kz.airbapay.apay_android.data.constant.saveCardData
 import kz.airbapay.apay_android.data.constant.sendCheckToEmail
@@ -47,6 +52,8 @@ internal fun HomePage(
     scrollState: ScrollState = rememberScrollState(),
 
 ) {
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+
     val context = LocalContext.current
     val showDialogExit = remember { mutableStateOf(false) }
     val switchSaveCard = remember { mutableStateOf(false) }
@@ -76,115 +83,129 @@ internal fun HomePage(
         showDialogExit.value = true
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(ColorsSdk.bgBlock)
-    ) {
-        ViewToolbar(
-            title = paymentOfPurchase(),
-            backIcon = R.drawable.ic_arrow_back,
-            actionBack = {
-                showDialogExit.value = true
-            }
-        )
+    Scaffold(
+        scaffoldState = scaffoldState
+    ) { padding ->
 
         Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxSize()
-                .verticalScroll(scrollState)
+                .fillMaxWidth()
                 .background(ColorsSdk.bgBlock)
-                .weight(1f)
         ) {
-            Spacer(modifier = Modifier.height(24.dp))
-            Image(
-                painter = painterResource(R.drawable.top_info),
-                contentDescription = "top_info",
+            ViewToolbar(
+                title = paymentOfPurchase(),
+                backIcon = R.drawable.ic_arrow_back,
+                actionBack = {
+                    showDialogExit.value = true
+                }
+            )
+
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .padding(horizontal = 46.dp)
-                    .height(32.dp)
-            )
-
-            TopInfoView()
-
-            Spacer(modifier = Modifier.height(16.dp))
-            CardNumberView(
-                cardNumberText = cardNumberText,
-                paySystemIcon = paySystemIcon,
-                cardNumberError = cardNumberError,
-                cardNumberFocusRequester = cardNumberFocusRequester,
-                nameHolderFocusRequester = nameHolderFocusRequester
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-            NameHolderView(
-                nameHolderText = nameHolderText,
-                nameHolderError = nameHolderError,
-                nameHolderFocusRequester = nameHolderFocusRequester,
-                dateExpiredFocusRequester = dateExpiredFocusRequester
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
-            Row(
-                modifier = Modifier
-                    .padding(start = 16.dp, end = 16.dp)
-                    .fillMaxWidth()
+                    .fillMaxSize()
+                    .verticalScroll(scrollState)
+                    .background(ColorsSdk.bgBlock)
+                    .weight(1f)
             ) {
-                DateExpiredView(
-                    dateExpiredFocusRequester = dateExpiredFocusRequester,
-                    dateExpiredError = dateExpiredError,
-                    dateExpiredText = dateExpiredText,
-                    cvvFocusRequester = cvvFocusRequester,
+                Spacer(modifier = Modifier.height(24.dp))
+                Image(
+                    painter = painterResource(R.drawable.top_info),
+                    contentDescription = "top_info",
                     modifier = Modifier
-                        .weight(0.5f)
-                        .padding(end = 6.dp)
+                        .padding(horizontal = 46.dp)
+                        .height(32.dp)
                 )
-                CvvView(
-                    cvvError = cvvError,
-                    cvvFocusRequester = cvvFocusRequester,
-                    cvvText = cvvText,
-                    emailFocusRequester = if(switchSendToEmail.value) emailFocusRequester else null,
+
+                TopInfoView()
+
+                Spacer(modifier = Modifier.height(16.dp))
+                CardNumberView(
+                    cardNumberText = cardNumberText,
+                    paySystemIcon = paySystemIcon,
+                    cardNumberError = cardNumberError,
+                    cardNumberFocusRequester = cardNumberFocusRequester,
+                    nameHolderFocusRequester = nameHolderFocusRequester
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                NameHolderView(
+                    nameHolderText = nameHolderText,
+                    nameHolderError = nameHolderError,
+                    nameHolderFocusRequester = nameHolderFocusRequester,
+                    dateExpiredFocusRequester = dateExpiredFocusRequester
+                )
+
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
                     modifier = Modifier
-                        .weight(0.5f)
-                        .padding(start = 6.dp)
+                        .padding(start = 16.dp, end = 16.dp)
+                        .fillMaxWidth()
+                ) {
+                    DateExpiredView(
+                        dateExpiredFocusRequester = dateExpiredFocusRequester,
+                        dateExpiredError = dateExpiredError,
+                        dateExpiredText = dateExpiredText,
+                        cvvFocusRequester = cvvFocusRequester,
+                        modifier = Modifier
+                            .weight(0.5f)
+                            .padding(end = 6.dp)
+                    )
+                    CvvView(
+                        cvvError = cvvError,
+                        cvvFocusRequester = cvvFocusRequester,
+                        cvvText = cvvText,
+                        emailFocusRequester = if (switchSendToEmail.value) emailFocusRequester else null,
+                        modifier = Modifier
+                            .weight(0.5f)
+                            .padding(start = 6.dp)
+                    )
+                }
+
+                Spacer(modifier = Modifier.height(24.dp))
+                SwitchedView(
+                    text = saveCardData(),
+                    switchCheckedState = switchSaveCard,
+                    actionOnTrue = { },
+                    actionOnFalse = { }
                 )
-            }
 
-            Spacer(modifier = Modifier.height(24.dp))
-            SwitchedView(
-                text = saveCardData(),
-                switchCheckedState = switchSaveCard,
-                actionOnTrue = { },
-                actionOnFalse = { }
-            )
-
-            SwitchedView(
-                text = sendCheckToEmail(),
-                switchCheckedState = switchSendToEmail,
-                actionOnTrue = { },
-                actionOnFalse = { }
-            )
-
-            if (switchSendToEmail.value) {
-                EmailView(
-                    emailText = emailText,
-                    emailError = emailError,
-                    emailFocusRequester = emailFocusRequester
+                SwitchedView(
+                    text = sendCheckToEmail(),
+                    switchCheckedState = switchSendToEmail,
+                    actionOnTrue = { },
+                    actionOnFalse = { }
                 )
-            }
 
-            BottomImages()
-        }
+                if (switchSendToEmail.value) {
+                    EmailView(
+                        emailText = emailText,
+                        emailError = emailError,
+                        emailFocusRequester = emailFocusRequester
+                    )
+                }
+
+                BottomImages()
+            }
 
         ConfirmButton("13 500")//todo
 
-        if (showDialogExit.value) {
-            InitDialogExit(
-                onDismissRequest = {
-                    showDialogExit.value = false
-                }
-            )
+            if (showDialogExit.value) {
+                InitDialogExit(
+                    onDismissRequest = {
+                        showDialogExit.value = false
+                    }
+                )
+            }
+        }
+
+        if (switchSaveCard.value) {
+            LaunchedEffect("snackBar") {
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = cardDataSaved(),
+                    actionLabel = null
+                )
+            }
         }
     }
 }
