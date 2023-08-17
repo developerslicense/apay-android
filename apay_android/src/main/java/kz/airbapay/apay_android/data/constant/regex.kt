7ac7221527be88112c22e070c043d8e9084bf02b
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-17 11:08:31 +0600

    мелкие правки

--- apay_android/src/main/java/kz/airbapay/apay_android/data/constant/regex.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/data/constant/regex.kt
@@ -6,7 +6,7 @@ internal object RegexConst {
   const val DIGITS = "\\b([0-9]+)"
   const val CARD = "\\b([0-9] )"
   const val TEXTS = "[a-zA-Z -]"
-  val numberCleanEn = "[^0-9.]";
-  val numberCleanRu = "[^0-9,.]";
+  val numberCleanEn = "[^0-9.]"
+  val numberCleanRu = "[^0-9,.]"
   val emailValidation = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$"
 }
