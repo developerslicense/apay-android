5d08ec5f8ac687f8a3ef89c8056091ad27765e71
Mikhail <mikhail.belikov@airbapay.kz>
2023-08-17 16:38:56 +0600

    добавил тесты

--- apay_android/src/main/java/kz/airbapay/apay_android/data/utils/card_utils/PaymentCard.kt
+++
@@ -1,9 +0,0 @@
-package kz.airbapay.apay_android.data.utils.card_utils
-
-internal class PaymentCard(
-    val type: CardType,
-    val number: String,
-    val name: String,
-    val month: Int,
-    val cvv: Int
-)
