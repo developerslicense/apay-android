ddc8165cf9514a4f5f60000a85c13152fc44e3d1
Mikhail <mikhail.belikov@airbapay.kz>
2024-01-11 14:00:37 +0600

    начал переделывать на новый флоу гугл пэй

--- apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/dialog/StartProcessingView.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/dialog/StartProcessingView.kt
@@ -2,6 +2,7 @@ package kz.airbapay.apay_android.ui.pages.dialog
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,6 +13,7 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,8 +21,9 @@ import androidx.compose.ui.layout.onSizeChanged
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import kotlinx.coroutines.launch
-import kz.airbapay.apay_android.AirbaPayActivity
+import kz.airbapay.apay_android.R
 import kz.airbapay.apay_android.data.constant.paymentByCard
 import kz.airbapay.apay_android.data.model.BankCard
 import kz.airbapay.apay_android.data.utils.DataHolder
@@ -28,40 +31,47 @@ import kz.airbapay.apay_android.network.api.Api
 import kz.airbapay.apay_android.network.base.provideRetrofit
 import kz.airbapay.apay_android.network.repository.AuthRepository
 import kz.airbapay.apay_android.network.repository.CardRepository
+import kz.airbapay.apay_android.network.repository.GooglePayRepository
+import kz.airbapay.apay_android.network.repository.PaymentsRepository
 import kz.airbapay.apay_android.network.repository.startAuth
 import kz.airbapay.apay_android.ui.pages.dialog.start_processing_ext.InitErrorState
 import kz.airbapay.apay_android.ui.pages.dialog.start_processing_ext.InitViewStartProcessingAmount
 import kz.airbapay.apay_android.ui.pages.dialog.start_processing_ext.InitViewStartProcessingButtonNext
 import kz.airbapay.apay_android.ui.pages.dialog.start_processing_ext.InitViewStartProcessingCards
-import kz.airbapay.apay_android.ui.pages.dialog.start_processing_ext.InitViewStartProcessingGPay
+import kz.airbapay.apay_android.ui.pages.googlepay.GooglePayPage
+import kz.airbapay.apay_android.ui.pages.home.bl.startPaymentProcessingGooglePay
 import kz.airbapay.apay_android.ui.resources.ColorsSdk
 import kz.airbapay.apay_android.ui.ui_components.BackHandler
 import kz.airbapay.apay_android.ui.ui_components.InitHeader
 import kz.airbapay.apay_android.ui.ui_components.ProgressBarView
+import kz.airbapay.apay_android.ui.ui_components.ViewToolbar
 
+// todo show Dialog only if hase changed
 @Composable
-internal fun StartProcessingView(
-    needShowProgressBar: Boolean = true,
+internal fun StartProcessingView( //todo rename StartProcessingPage, startprocessingpage. move Dialog in dialog
     actionClose: () -> Unit,
     actionOnLoadingCompleted: () -> Unit = {},
-    isBottomSheetType: Boolean = true,
     backgroundColor: Color = ColorsSdk.bgBlock,
     isAuthenticated: MutableState<Boolean>,
     customSuccessPage: @Composable (() -> Unit)? = null,
-    sheetStateVisible: Boolean
+    navController: NavController?
 ) {
     val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
     val retrofit = provideRetrofit(context)
+    val redirectUrl = remember {
+        mutableStateOf<String?>(null)
+    }
 
     if (retrofit != null) {
         val api = retrofit.create(Api::class.java)
+        val paymentRepository = PaymentsRepository(api)
         val cardRepository = CardRepository(api)
         val authRepository = AuthRepository(api)
+        val googlePayRepository = GooglePayRepository(api)
 
-        if (isBottomSheetType && sheetStateVisible) {
-            BackHandler {
-                actionClose()
-            }
+        BackHandler {
+            actionClose()
         }
 
         val purchaseAmount = DataHolder.purchaseAmountFormatted.collectAsState()
@@ -78,71 +88,64 @@ internal fun StartProcessingView(
         Card(
             backgroundColor = backgroundColor,
             shape = RoundedCornerShape(
-                topStart = if (isBottomSheetType) 12.dp else 0.dp,
-                topEnd = if (isBottomSheetType) 12.dp else 0.dp
+                topStart = 0.dp,
+                topEnd = 0.dp
             ),
             modifier = Modifier
-                .fillMaxWidth()
+                .fillMaxSize()
                 .onSizeChanged {
                     size.value = it
                 },
-            elevation = if (isBottomSheetType) 5.dp else 0.dp
+            elevation = 0.dp
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+            if (!isLoading.value) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
 
-                if (isBottomSheetType) {
-                    InitHeader(
+                    ViewToolbar(
                         title = paymentByCard(),
-                        actionClose = actionClose,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(ColorsSdk.gray0)
+                        backIcon = R.drawable.ic_arrow_back,
+                        actionBack = actionClose
                     )
-                }
 
-                if (isError.value) {
-                    InitErrorState()
+                    if (isError.value) {
+                        InitErrorState()
 
-                } else {
-                    InitViewStartProcessingAmount(purchaseAmount.value)
-                    InitViewStartProcessingGPay(
-                        openGooglePay = {
-                            actionClose()
-                            AirbaPayActivity.init(
-                                context = context,
-                                customSuccessPage = customSuccessPage,
-                                isGooglePay = true
+                    } else {
+                        InitViewStartProcessingAmount(purchaseAmount.value)
+
+                        if (redirectUrl.value != null) {
+                            GooglePayPage(
+                                url = redirectUrl.value,
+                                navController = navController
                             )
                         }
-                    )
 
-                    if (savedCards.value.isNotEmpty()
-                        && isAuthenticated.value
-                    ) {
-                        InitViewStartProcessingCards(
+                        if (savedCards.value.isNotEmpty()
+                            && isAuthenticated.value
+                        ) {
+                            InitViewStartProcessingCards(
+                                savedCards = savedCards.value,
+                                selectedCard = selectedCard,
+                                actionClose = actionClose,
+                                customSuccessPage = customSuccessPage
+                            )
+                        }
+
+                        InitViewStartProcessingButtonNext(
                             savedCards = savedCards.value,
-                            selectedCard = selectedCard,
                             actionClose = actionClose,
+                            purchaseAmount = purchaseAmount.value,
+                            isAuthenticated = isAuthenticated.value,
+                            selectedCard = selectedCard,
                             customSuccessPage = customSuccessPage
                         )
                     }
-
-                    InitViewStartProcessingButtonNext(
-                        savedCards = savedCards.value,
-                        actionClose = actionClose,
-                        purchaseAmount = purchaseAmount.value,
-                        isAuthenticated = isAuthenticated.value,
-                        selectedCard = selectedCard,
-                        customSuccessPage = customSuccessPage
-                    )
                 }
             }
 
-            if (isLoading.value
-                && needShowProgressBar
-            ) {
+            if (isLoading.value) {
                 ProgressBarView(
                     size = size,
                     modifier = Modifier.wrapContentHeight()
@@ -162,6 +165,16 @@ internal fun StartProcessingView(
                         actionOnLoadingCompleted()
                     },
                     onResult = {
+                        startPaymentProcessingGooglePay(
+                            redirectUrl = redirectUrl,
+                            coroutineScope = coroutineScope,
+                            paymentsRepository = paymentRepository,
+                            googlePayRepository = googlePayRepository,
+                            authRepository = authRepository,
+                            navController = navController,
+
+                        )
+
                         cardRepository.getCards(
                             accountId = DataHolder.accountId,
                             error = {
@@ -186,25 +199,24 @@ internal fun StartProcessingView(
         Card(
             backgroundColor = backgroundColor,
             shape = RoundedCornerShape(
-                topStart = if (isBottomSheetType) 12.dp else 0.dp,
-                topEnd = if (isBottomSheetType) 12.dp else 0.dp
+                topStart = 0.dp,
+                topEnd = 0.dp
             ),
             modifier = Modifier.fillMaxWidth(),
-            elevation = if (isBottomSheetType) 5.dp else 0.dp
+            elevation = 0.dp
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                if (isBottomSheetType) {
-                    InitHeader(
-                        title = paymentByCard(),
-                        actionClose = actionClose,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .background(ColorsSdk.gray0)
-                    )
-                }
+                InitHeader(
+                    title = paymentByCard(),
+                    actionClose = actionClose,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(ColorsSdk.gray0)
+                )
+
                 InitErrorState()
             }
         }
