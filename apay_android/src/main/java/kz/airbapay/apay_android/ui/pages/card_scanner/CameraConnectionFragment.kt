bd95d65e24c471bbb1d48e31ba919c23bbc02ea6
Mikhail <mikhail.belikov@airbapay.kz>
2024-02-01 15:36:30 +0600

    рефакторинг

---
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/card_scanner/CameraConnectionFragment.kt
@@ -0,0 +1,435 @@
+package kz.airbapay.apay_android.ui.pages.card_scanner
+
+import android.app.Activity
+import android.content.Context
+import android.content.res.Configuration
+import android.graphics.ImageFormat
+import android.graphics.Matrix
+import android.graphics.RectF
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.hardware.camera2.CaptureResult
+import android.hardware.camera2.TotalCaptureResult
+import android.media.ImageReader
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Size
+import android.util.SparseIntArray
+import android.view.LayoutInflater
+import android.view.Surface
+import android.view.TextureView.SurfaceTextureListener
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import kz.airbapay.apay_android.R
+import kz.airbapay.apay_android.ui.pages.card_scanner.camera.ConnectionCallback
+import kz.airbapay.apay_android.ui.pages.card_scanner.view.AutoFitTextureView
+import java.util.Collections
+import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
+
+open class CameraConnectionFragment private constructor(
+    private val cameraConnectionCallback: ConnectionCallback,
+    private val imageListener: ImageReader.OnImageAvailableListener,
+    private val layout: Int,
+    private val inputSize: Size
+) : Fragment() {
+    private val cameraOpenCloseLock = Semaphore(1)
+    private val captureCallback: CameraCaptureSession.CaptureCallback =
+        object : CameraCaptureSession.CaptureCallback() {
+            override fun onCaptureProgressed(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                partialResult: CaptureResult
+            ) {
+            }
+
+            override fun onCaptureCompleted(
+                session: CameraCaptureSession,
+                request: CaptureRequest,
+                result: TotalCaptureResult
+            ) {
+            }
+        }
+    private var cameraId: String? = null
+    private var textureView: AutoFitTextureView? = null
+    private var captureSession: CameraCaptureSession? = null
+    private var cameraDevice: CameraDevice? = null
+    private var sensorOrientation: Int? = null
+    private var previewSize: Size? = null
+    private var backgroundThread: HandlerThread? = null
+    private var backgroundHandler: Handler? = null
+    private var previewReader: ImageReader? = null
+    private var previewRequestBuilder: CaptureRequest.Builder? = null
+    private var previewRequest: CaptureRequest? = null
+    private val stateCallback: CameraDevice.StateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(cd: CameraDevice) {
+            // This method is called when the camera is opened.  We start camera preview here.
+            cameraOpenCloseLock.release()
+            cameraDevice = cd
+            createCameraPreviewSession()
+        }
+
+        override fun onDisconnected(cd: CameraDevice) {
+            cameraOpenCloseLock.release()
+            cd.close()
+            cameraDevice = null
+        }
+
+        override fun onError(cd: CameraDevice, error: Int) {
+            cameraOpenCloseLock.release()
+            cd.close()
+            cameraDevice = null
+            val activity: Activity? = activity
+            activity?.finish()
+        }
+    }
+
+    /**
+     * [TextureView.SurfaceTextureListener] handles several lifecycle events on a [ ].
+     */
+    private val surfaceTextureListener: SurfaceTextureListener = object : SurfaceTextureListener {
+        override fun onSurfaceTextureAvailable(
+            texture: SurfaceTexture, width: Int, height: Int
+        ) {
+            openCamera(width, height)
+        }
+
+        override fun onSurfaceTextureSizeChanged(
+            texture: SurfaceTexture, width: Int, height: Int
+        ) {
+            configureTransform(width, height)
+        }
+
+        override fun onSurfaceTextureDestroyed(texture: SurfaceTexture): Boolean {
+            return true
+        }
+
+        override fun onSurfaceTextureUpdated(texture: SurfaceTexture) {}
+    }
+
+    /**
+     * Shows a [Toast] on the UI thread.
+     */
+    private fun showToast() {
+        val activity: Activity? = activity
+        activity?.runOnUiThread { Toast.makeText(activity, "Failed", Toast.LENGTH_SHORT).show() }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layout, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        textureView = view.findViewById<View>(R.id.texture) as AutoFitTextureView
+    }
+
+    override fun onResume() {
+        super.onResume()
+        startBackgroundThread()
+        if (textureView!!.isAvailable) {
+            openCamera(textureView!!.width, textureView!!.height)
+        } else {
+            textureView!!.surfaceTextureListener = surfaceTextureListener
+        }
+    }
+
+    override fun onPause() {
+        closeCamera()
+        stopBackgroundThread()
+        super.onPause()
+    }
+
+    fun setCamera(cameraId: String?) {
+        this.cameraId = cameraId
+    }
+
+    /**
+     * Sets up member variables related to camera.
+     */
+    private fun setUpCameraOutputs() {
+        val activity: Activity = requireActivity()
+        val manager = activity.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            val characteristics = manager.getCameraCharacteristics(cameraId!!)
+            val map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+            sensorOrientation = characteristics.get(CameraCharacteristics.SENSOR_ORIENTATION)
+
+            // Danger, W.R.! Attempting to use too large a preview size could  exceed the camera
+            // bus' bandwidth limitation, resulting in gorgeous previews but the storage of
+            // garbage capture data.
+            previewSize = chooseOptimalSize(
+                if (map != null) map.getOutputSizes(SurfaceTexture::class.java) else arrayOfNulls(0),
+                inputSize.width,
+                inputSize.height
+            )
+
+            // We fit the aspect ratio of TextureView to the size of preview we picked.
+            val orientation = resources.configuration.orientation
+            if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                textureView!!.setAspectRatio(previewSize!!.width, previewSize!!.height)
+            } else {
+                textureView!!.setAspectRatio(previewSize!!.height, previewSize!!.width)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            showToast()
+        }
+        cameraConnectionCallback.onPreviewSizeChosen(previewSize!!, sensorOrientation!!)
+    }
+
+    /**
+     * Opens the camera specified by [CameraConnectionFragment.cameraId].
+     */
+    private fun openCamera(width: Int, height: Int) {
+        setUpCameraOutputs()
+        configureTransform(width, height)
+        val activity: Activity? = activity
+        val manager = activity!!.getSystemService(Context.CAMERA_SERVICE) as CameraManager
+        try {
+            if (!cameraOpenCloseLock.tryAcquire(2500, TimeUnit.MILLISECONDS)) {
+                throw RuntimeException("Time out waiting to lock camera opening.")
+            }
+            manager.openCamera(cameraId!!, stateCallback, backgroundHandler)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Closes the current [CameraDevice].
+     */
+    private fun closeCamera() {
+        try {
+            cameraOpenCloseLock.acquire()
+            if (null != captureSession) {
+                captureSession!!.close()
+                captureSession = null
+            }
+            if (null != cameraDevice) {
+                cameraDevice!!.close()
+                cameraDevice = null
+            }
+            if (null != previewReader) {
+                previewReader!!.close()
+                previewReader = null
+            }
+        } catch (e: InterruptedException) {
+            throw RuntimeException("Interrupted while trying to lock camera closing.", e)
+        } finally {
+            cameraOpenCloseLock.release()
+        }
+    }
+
+    /**
+     * Starts a background thread and its [Handler].
+     */
+    private fun startBackgroundThread() {
+        backgroundThread = HandlerThread("ImageListener")
+        backgroundThread!!.start()
+        backgroundHandler = Handler(backgroundThread!!.looper)
+    }
+
+    /**
+     * Stops the background thread and its [Handler].
+     */
+    private fun stopBackgroundThread() {
+        backgroundThread!!.quitSafely()
+        try {
+            backgroundThread!!.join()
+            backgroundThread = null
+            backgroundHandler = null
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Creates a new [CameraCaptureSession] for camera preview.
+     */
+    private fun createCameraPreviewSession() {
+        try {
+            val texture = textureView!!.surfaceTexture!!
+
+            // We configure the size of default buffer to be the size of camera preview we want.
+            texture.setDefaultBufferSize(previewSize!!.width, previewSize!!.height)
+
+            // This is the output Surface we need to start preview.
+            val surface = Surface(texture)
+
+            // We set up a CaptureRequest.Builder with the output Surface.
+            previewRequestBuilder =
+                cameraDevice!!.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+            previewRequestBuilder!!.addTarget(surface)
+
+            // Create the reader for the preview frames.
+            previewReader = ImageReader.newInstance(
+                previewSize!!.width, previewSize!!.height, ImageFormat.YUV_420_888, 2
+            )
+            previewReader!!.setOnImageAvailableListener(imageListener, backgroundHandler)
+            previewRequestBuilder!!.addTarget(previewReader!!.surface)
+
+            // Here, we create a CameraCaptureSession for camera preview.
+            cameraDevice!!.createCaptureSession(
+                listOf(surface, previewReader!!.surface),
+                object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(cameraCaptureSession: CameraCaptureSession) {
+                        // The camera is already closed
+                        if (null == cameraDevice) {
+                            return
+                        }
+
+                        // When the session is ready, we start displaying the preview.
+                        captureSession = cameraCaptureSession
+                        try {
+                            // Auto focus should be continuous for camera preview.
+                            previewRequestBuilder!!.set(
+                                CaptureRequest.CONTROL_AF_MODE,
+                                CaptureRequest.CONTROL_AF_MODE_CONTINUOUS_PICTURE
+                            )
+                            // Flash is automatically enabled when necessary.
+                            previewRequestBuilder!!.set(
+                                CaptureRequest.CONTROL_AE_MODE,
+                                CaptureRequest.CONTROL_AE_MODE_ON_AUTO_FLASH
+                            )
+
+                            // Finally, we start displaying the camera preview.
+                            previewRequest = previewRequestBuilder!!.build()
+                            captureSession!!.setRepeatingRequest(
+                                previewRequest!!, captureCallback, backgroundHandler
+                            )
+                        } catch (e: CameraAccessException) {
+                            e.printStackTrace()
+                        }
+                    }
+
+                    override fun onConfigureFailed(cameraCaptureSession: CameraCaptureSession) {
+                        showToast()
+                    }
+                },
+                null
+            )
+        } catch (e: CameraAccessException) {
+            e.printStackTrace()
+        }
+    }
+
+    /**
+     * Configures the necessary [Matrix] transformation to `mTextureView`. This method should be
+     * called after the camera preview size is determined in setUpCameraOutputs and also the size of
+     * `mTextureView` is fixed.
+     *
+     * @param viewWidth  The width of `mTextureView`
+     * @param viewHeight The height of `mTextureView`
+     */
+    private fun configureTransform(viewWidth: Int, viewHeight: Int) {
+        val activity: Activity? = activity
+        if (null == textureView || null == previewSize || null == activity) {
+            return
+        }
+        val rotation = activity.windowManager.defaultDisplay.rotation
+        val matrix = Matrix()
+        val viewRect = RectF(0f, 0f, viewWidth.toFloat(), viewHeight.toFloat())
+        val bufferRect = RectF(0f, 0f, previewSize!!.height.toFloat(), previewSize!!.width.toFloat())
+        val centerX = viewRect.centerX()
+        val centerY = viewRect.centerY()
+
+        if (Surface.ROTATION_90 == rotation || Surface.ROTATION_270 == rotation) {
+            bufferRect.offset(centerX - bufferRect.centerX(), centerY - bufferRect.centerY())
+            matrix.setRectToRect(viewRect, bufferRect, Matrix.ScaleToFit.FILL)
+            val scale = Math.max(
+                viewHeight.toFloat() / previewSize!!.height,
+                viewWidth.toFloat() / previewSize!!.width
+            )
+            matrix.postScale(scale, scale, centerX, centerY)
+            matrix.postRotate((90 * (rotation - 2)).toFloat(), centerX, centerY)
+
+        } else if (Surface.ROTATION_180 == rotation) {
+            matrix.postRotate(180f, centerX, centerY)
+        }
+        textureView!!.setTransform(matrix)
+    }
+
+    /**
+     * Compares two `Size`s based on their areas.
+     */
+    internal class CompareSizesByArea : Comparator<Size> {
+        override fun compare(lhs: Size, rhs: Size): Int {
+            // We cast here to ensure the multiplications won't overflow
+            return java.lang.Long.signum(
+                lhs.width.toLong() * lhs.height - rhs.width.toLong() * rhs.height
+            )
+        }
+    }
+
+    companion object {
+        private const val MINIMUM_PREVIEW_SIZE = 320
+        private val ORIENTATIONS = SparseIntArray()
+
+        init {
+            ORIENTATIONS.append(Surface.ROTATION_0, 90)
+            ORIENTATIONS.append(Surface.ROTATION_90, 0)
+            ORIENTATIONS.append(Surface.ROTATION_180, 270)
+            ORIENTATIONS.append(Surface.ROTATION_270, 180)
+        }
+
+        /**
+         * Given `choices` of `Size`s supported by a camera, chooses the smallest one whose
+         * width and height are at least as large as the minimum of both, or an exact match if possible.
+         *
+         * @param choices The list of sizes that the camera supports for the intended output class
+         * @param width   The minimum desired width
+         * @param height  The minimum desired height
+         * @return The optimal `Size`, or an arbitrary one if none were big enough
+         */
+        protected fun chooseOptimalSize(choices: Array<Size?>, width: Int, height: Int): Size {
+            val minSize = Math.max(Math.min(width, height), MINIMUM_PREVIEW_SIZE)
+            val desiredSize = Size(width, height)
+
+            // Collect the supported resolutions that are at least as big as the preview Surface
+            var exactSizeFound = false
+            val bigEnough: MutableList<Size> = ArrayList()
+            val tooSmall: MutableList<Size> = ArrayList()
+            for (option in choices) {
+                if (option == desiredSize) {
+                    // Set the size but don't return yet so that remaining sizes will still be logged.
+                    exactSizeFound = true
+                }
+                if (option!!.height >= minSize && option.width >= minSize) {
+                    bigEnough.add(option)
+                } else {
+                    tooSmall.add(option)
+                }
+            }
+            if (exactSizeFound) {
+                return desiredSize
+            }
+
+            // Pick the smallest of those, assuming we found any
+            return if (bigEnough.size > 0) {
+                Collections.min(bigEnough, CompareSizesByArea())
+            } else {
+                choices[0]!!
+            }
+        }
+
+        @JvmStatic
+        fun newInstance(
+            callback: ConnectionCallback,
+            imageListener: ImageReader.OnImageAvailableListener,
+            layout: Int,
+            inputSize: Size
+        ): CameraConnectionFragment {
+            return CameraConnectionFragment(callback, imageListener, layout, inputSize)
+        }
+    }
+}
--- apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/card_scanner/camera/ConnectionCallback.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/card_scanner/camera/ConnectionCallback.kt
@@ -2,6 +2,6 @@ package kz.airbapay.apay_android.ui.pages.card_scanner.camera
 
 import android.util.Size
 
-internal interface ConnectionCallback {
+interface ConnectionCallback {
     fun onPreviewSizeChosen(size: Size, cameraRotation: Int)
 }
