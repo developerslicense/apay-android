213ead743017ddfcee6b56d5da32edb8a2b3205a
Mikhail <mikhail.belikov@airbapay.kz>
2024-04-16 12:13:52 +0500

    удалил комменты

--- apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/googlepay/nativegp/GooglePayUtil.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/ui/pages/googlepay/nativegp/GooglePayUtil.kt
@@ -25,35 +25,12 @@ import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 
-/**
- * Contains helper static methods for dealing with the Payments API.
- *
- * Many of the parameters used in the code are optional and are set here merely to call out their
- * existence. Please consult the documentation to learn more and feel free to remove ones not
- * relevant to your implementation.
- */
 object GooglePayUtil {
 
-    /**
-     * Create a Google Pay API base request object with properties used in all requests.
-     *
-     * @return Google Pay API base request object.
-     * @throws JSONException
-     */
     private val baseRequest = JSONObject()
         .put("apiVersion", 2)
         .put("apiVersionMinor", 0)
 
-    /**
-     * Gateway Integration: Identify your gateway and your app's gateway merchant identifier.
-     *
-     *
-     * The Google Pay API response will return an encrypted payment method capable of being charged
-     * by a supported gateway after payer authorization.
-     * @return Payment data tokenization for the CARD payment method.
-     * @throws JSONException
-     * See [PaymentMethodTokenizationSpecification](https://developers.google.com/pay/api/android/reference/object.PaymentMethodTokenizationSpecification)
-     */
     private fun gatewayTokenizationSpecification(): JSONObject =
         JSONObject()
             .put("type", "PAYMENT_GATEWAY")
@@ -65,26 +42,12 @@ object GooglePayUtil {
                 )
             ))
 
-    /**
-     * Card networks supported by your app and your gateway.
-     * @return Allowed card networks
-     * See [CardParameters](https://developers.google.com/pay/api/android/reference/object.CardParameters)
-     */
     private val allowedCardNetworks = JSONArray(listOf(
         "AMEX",
-//        "DISCOVER",
-//        "JCB",
         "MASTERCARD",
         "VISA")
     )
 
-    /**
-     * Card authentication methods supported by your app and your gateway.
-     *
-     *
-     * @return Allowed card authentication methods.
-     * See [CardParameters](https://developers.google.com/pay/api/android/reference/object.CardParameters)
-     */
     private val allowedCardAuthMethods = JSONArray(
         listOf(
             "PAN_ONLY",
@@ -92,18 +55,6 @@ object GooglePayUtil {
         )
     )
 
-    /**
-     * Describe your app's support for the CARD payment method.
-     *
-     *
-     * The provided properties are applicable to both an IsReadyToPayRequest and a
-     * PaymentDataRequest.
-     *
-     * @return A CARD PaymentMethod object describing accepted cards.
-     * @throws JSONException
-     * See [PaymentMethod](https://developers.google.com/pay/api/android/reference/object.PaymentMethod)
-     */
-    // Optionally, you can add billing address/phone number associated with a CARD payment method.
     private fun baseCardPaymentMethod(): JSONObject =
         JSONObject()
             .put("type", "CARD")
@@ -118,25 +69,11 @@ object GooglePayUtil {
                     )
             )
 
-    /**
-     * Describe the expected returned payment data for the CARD payment method
-     *
-     * @return A CARD PaymentMethod describing accepted cards and optional fields.
-     * @throws JSONException
-     * See [PaymentMethod](https://developers.google.com/pay/api/android/reference/object.PaymentMethod)
-     */
     private fun cardPaymentMethod(): JSONObject = baseCardPaymentMethod()
         .put("tokenizationSpecification", gatewayTokenizationSpecification())
 
     fun allowedPaymentMethods(): JSONArray = JSONArray().put(cardPaymentMethod())
 
-    /**
-     * An object describing accepted forms of payment by your app, used to determine a viewer's
-     * readiness to pay.
-     *
-     * @return API version and payment methods supported by the app.
-     * See [IsReadyToPayRequest](https://developers.google.com/pay/api/android/reference/object.IsReadyToPayRequest)
-     */
     fun isReadyToPayRequest(): JSONObject? =
         try {
             baseRequest
@@ -145,20 +82,9 @@ object GooglePayUtil {
             null
         }
 
-    /**
-     * Information about the merchant requesting payment information
-     *
-     * @return Information about the merchant.
-     * @throws JSONException
-     * See [MerchantInfo](https://developers.google.com/pay/api/android/reference/object.MerchantInfo)
-     */
     private val merchantInfo: JSONObject =
         JSONObject().put("merchantName", "AirbaPay")
 
-    /**
-     * Creates an instance of [PaymentsClient]
-     * @param context from the caller activity.
-     */
     fun createPaymentsClient(context: Context): PaymentsClient {
         val walletOptions = Wallet.WalletOptions.Builder()
             .setEnvironment(if(DataHolder.isProd) WalletConstants.ENVIRONMENT_PRODUCTION else WalletConstants.ENVIRONMENT_TEST)
@@ -167,13 +93,6 @@ object GooglePayUtil {
         return Wallet.getPaymentsClient(context, walletOptions)
     }
 
-    /**
-     * Provide Google Pay API with a payment amount, currency, and amount status.
-     *
-     * @return information about the requested payment.
-     * @throws JSONException
-     * See [TransactionInfo](https://developers.google.com/pay/api/android/reference/object.TransactionInfo)
-     */
     private fun getTransactionInfo(price: String): JSONObject =
         JSONObject()
             .put("totalPrice", price)
@@ -181,12 +100,6 @@ object GooglePayUtil {
             .put("countryCode", "KZ")
             .put("currencyCode", "KZT")
 
-    /**
-     * An object describing information requested in a Google Pay payment sheet
-     *
-     * @return Payment data expected by your app.
-     * See [PaymentDataRequest](https://developers.google.com/pay/api/android/reference/object.PaymentDataRequest)
-     */
     fun getPaymentDataRequest(price: String): JSONObject =
         baseRequest
             .put("allowedPaymentMethods", allowedPaymentMethods())
