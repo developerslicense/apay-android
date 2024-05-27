fee077453fdb6da8b0693a3f56826444a74ad4b1
Mikhail <mikhail.belikov@airbapay.kz>
2024-05-27 14:51:10 +0500

    сменил номер сборки

--- apay_android/src/main/java/kz/airbapay/apay_android/data/utils/DataHolder.kt
+++ apay_android/src/main/java/kz/airbapay/apay_android/data/utils/DataHolder.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kz.airbapay.apay_android.AirbaPaySdk
 
 internal object DataHolder {
-  const val sdkVersion: String = "2.0.2"
+  const val sdkVersion: String = "2.0.3"
   var baseUrl = ""
 
   var connectTimeout = 60
